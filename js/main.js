fad0220bf8128b2d0c9ff0b7bf7d7dc3742ee159
Deepkumar Patel <dspguru97@gmail.com>
2020-03-08 22:22:24 -0400

    responsiveNavBarDone

--- js/main.js
+++ js/main.js
@@ -2,10 +2,26 @@
 
 const burger = document.querySelector('.burgerMenu');
 const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
 
 function openMenu()
 {
+    // Toggle Nav
     nav.classList.toggle('nav-active');
+
+    //Animating Links - Left to Right
+    navLinks.forEach((link,index) =>{
+        if(link.style.animation)
+        {
+          link.style.animation = '';
+        }
+        else
+        {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.8}s`;
+        }
+    });
+
+    burger.classList.toggle('toggle');
 }
 
 
